f55adbd3cd8067974e1dc32d047e757d561c4b57
Kiril <csgocvadimkiril80@gmail.com>
2026-01-22 19:42:56 +0100

    save

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cargo bootimage
-nasm -f elf64 ./src/task/cpu_info/cpuinfo.asm -o ./src/task/cpu_info/cpu_info.o
+#nasm -f elf64 ./src/task/cpu_info/cpuinfo.asm -o ./src/task/cpu_info/cpu_info.o
 qemu-system-x86_64 -drive format=raw,file=target/x86_64-kernel/debug/bootimage-Runix.bin
