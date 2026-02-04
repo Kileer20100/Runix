a7c84214c1b07305bae48619a3b70ac1aa958358
KS <120547170+Kileer20100@users.noreply.github.com>
2026-02-04 02:32:05 +0100

    Delete kernel/target directory

--- kernel/target/release/build/Runix-aff3449e57669889/build_script_build-aff3449e57669889.d
+++
@@ -1,5 +0,0 @@
-/home/kiril/Rust/Runix/kernel/target/release/build/Runix-aff3449e57669889/build_script_build-aff3449e57669889.d: build.rs
-
-/home/kiril/Rust/Runix/kernel/target/release/build/Runix-aff3449e57669889/build_script_build-aff3449e57669889: build.rs
-
-build.rs:
--- kernel/target/x86_64-unknown-none/release/deps/scopeguard-b9a10509ccbae750.d
+++
@@ -1,7 +0,0 @@
-/home/kiril/Rust/Runix/kernel/target/x86_64-unknown-none/release/deps/scopeguard-b9a10509ccbae750.d: /home/kiril/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/kiril/Rust/Runix/kernel/target/x86_64-unknown-none/release/deps/libscopeguard-b9a10509ccbae750.rlib: /home/kiril/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/kiril/Rust/Runix/kernel/target/x86_64-unknown-none/release/deps/libscopeguard-b9a10509ccbae750.rmeta: /home/kiril/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs
-
-/home/kiril/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/scopeguard-1.2.0/src/lib.rs:
